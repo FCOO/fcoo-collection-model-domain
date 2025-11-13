210983ee06acbfc7c4d15d6814d7af219dab016a
Niels Holt <niels@steenbuchholt.dk>
2025-11-13 10:44:48 +0100

    Bug fix when used with multi-maps in fcoo-maps

--- dist/fcoo-collection-model-domain.js
+++ dist/fcoo-collection-model-domain.js
@@ -1100,7 +1100,7 @@ Create collections and datasets
             //Create background-layer and use the color-event to update time-range info
             let backgroundId = options.backgroundId || 'standard';
             e.map.setBackground(backgroundId);
-
+            e.map.isVisibleInMultiMaps = true; //Needed to fire 'color' event....
             if (options.timeRange)
                 e.map.backgroundLandLayer.on('color', this.updateDisplayStatus.bind(this) );
 
